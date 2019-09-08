7b36030ddb2eff7206bfd7c124c0a6a5af0df6f3
DylanJu <benefit.joo@gmail.com>
2019-09-08 15:39:51 +0900

    Stpe1: Added react, typescript, webpack, babel

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  
+  entry: './src/index.tsx',
+  
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 3000,
+    hot: true,
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', 'jsx', '.ts', '.tsx'],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index_dev.html',
+    }),
+  ],
+}
