e6eb4dc3baa7bb56ec0ac60c74d23e5c75ecd8a3
dylanju <benefit.joo@gmail.com>
2020-02-01 16:06:13 +0900

    Added css and absolute path config to wds

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,15 +22,27 @@ module.exports = {
         test: /\.tsx?$/,
         use: ['babel-loader', 'ts-loader'],
       },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
   },
 
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      pages: path.resolve('src/pages/'),
+      components: path.resolve('src/components/'),
+      actions: path.resolve('src/store/actions/'),
+      reducers: path.resolve('src/store/reducers/'),
+      util: path.resolve('src/util/'),
+    },
   },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'public/index_dev.html',
